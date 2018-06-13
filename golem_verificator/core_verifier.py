eb6e2888086f80cc74903074d82131f43488e0aa
Elfoniok <lukaszfoniok@gmail.com>
2018-06-13 09:56:12 +0200

    Verification queue moved out

--- golem_verificator/core_verifier.py
+++ golem_verificator/core_verifier.py
@@ -1,21 +1,12 @@
 import logging
-import queue
 import os
 import threading
-
-from collections import namedtuple
 from datetime import datetime
-from types import FunctionType
-from typing import Optional, Type, Dict
-
-from twisted.internet.defer import Deferred, gatherResults
-
 from .common.common import deadline_to_timeout
-from .common.localcomputer import ComputerAdapter
-
 from .verifier import (StateVerifier, SubtaskVerificationState, Verifier)
 
-logger = logging.getLogger("apps.core")
+logger = logging.getLogger("golem_verificator.core_verifier")
+
 
 class CoreVerifier(StateVerifier):
 
@@ -28,6 +19,9 @@ class CoreVerifier(StateVerifier):
         self._check_files(subtask_info, results, reference_data, resources)
 
     def _check_files(self, subtask_info, results, reference_data, resources):
+        import pdb
+        pdb.set_trace()
+
         for result in results:
             if os.path.isfile(result):
                 if self._verify_result(subtask_info, result, reference_data,
@@ -52,100 +46,4 @@ class CoreVerifier(StateVerifier):
         """ Override this to change verification method
         """
         return True
-
-
-class VerificationQueue:
-
-    Entry = namedtuple('Entry', ['verifier_class', 'subtask_id',
-                                 'deadline', 'kwargs', 'cb'])
-
-    def __init__(self, concurrency: int = 1) -> None:
-
-        self._concurrency = concurrency
-        self._queue: queue.Queue = queue.Queue()
-
-        self._lock = threading.Lock()
-        self._jobs: Dict[str, Deferred] = dict()
-        self._paused = False
-
-    def submit(self,
-               verifier_class: Type[Verifier],
-               subtask_id: str,
-               deadline: int,
-               cb: FunctionType,
-               **kwargs) -> None:
-
-        entry = self.Entry(verifier_class, subtask_id, deadline, kwargs, cb)
-        self._queue.put(entry)
-        self._process_queue()
-
-    def pause(self) -> Deferred:
-        self._paused = True
-        deferred_list = list(self._jobs.values())
-        return gatherResults(deferred_list)
-
-    def resume(self) -> None:
-        self._paused = False
-        self._process_queue()
-
-    @property
-    def can_run(self) -> bool:
-        with self._lock:
-            return not self._paused and len(self._jobs) < self._concurrency
-
-    def _process_queue(self) -> None:
-        if self.can_run:
-            entry = self._next()
-            if entry:
-                self._run(entry)
-
-    def _next(self) -> Optional['Entry']:
-        try:
-            return self._queue.get(block=False)
-        except queue.Empty:
-            return None
-
-    def _run(self, entry: Entry) -> None:
-        deferred_job = Deferred()
-        subtask_id = entry.subtask_id
-
-        with self._lock:
-            self._jobs[subtask_id] = deferred_job
-
-        logger.info("Running verification of subtask %r", subtask_id)
-
-        def callback(*args, **kwargs):
-            with self._lock:
-                deferred_job.callback(True)
-                self._jobs.pop(subtask_id, None)
-
-            logger.info("Finished verification of subtask %r", subtask_id)
-            try:
-                entry.cb(*args, **kwargs)
-            finally:
-                self._process_queue()
-
-        try:
-            verifier = entry.verifier_class(callback)
-            #FIXME set this from golem core, this code should not know about LocalComputer
-            # or cmputer adapter. Maybe create an interface object.
-            verifier.computer = ComputerAdapter()
-            if deadline_to_timeout(entry.deadline) > 0:
-                verifier.start_verification(**entry.kwargs)
-            else:
-                verifier.task_timeout(subtask_id)
-                raise Exception("Task deadline passed")
-
-        except Exception as exc:  # pylint: disable=broad-except
-            with self._lock:
-                deferred_job.errback(exc)
-                self._jobs.pop(subtask_id, None)
-
-            logger.error("Failed to start verification of subtask %r: %r",
-                         subtask_id, exc)
-            self._process_queue()
-
-
-    def _reset(self) -> None:
-        self._queue = queue.Queue()
-        self._jobs = dict()
+        
