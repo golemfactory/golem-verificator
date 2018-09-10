ac208fa73cf4bf2b9a69bb0d1d5af8b3c2f4e2dd
creed <michal.szostakiewicz@imapp.pl>
2018-09-10 17:16:37 +0200

    cropping and comparing

--- golem_verificator/docker/blender/images/scripts/redundancy/subtask.py
+++ golem_verificator/docker/blender/images/scripts/redundancy/subtask.py
@@ -8,4 +8,8 @@ class Subtask:
 
     def find_intersection( self, other ):
         intersection_rect = self.crop_rect.intersect( other.crop_rect )
-        
+
+        self_cropped = self.crop_img.crop( intersection_rect - self.crop_rect.topleft() )
+        other_cropped = other.crop_img.crop( intersection_rect - other.crop_rect.topleft() )
+
+        return compare_images( self_cropped, other_cropped )
