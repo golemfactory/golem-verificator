95d6c67c7a878182c5a53ea4394fb66e1dc5ba8e
Elfoniok <lukaszfoniok@gmail.com>
2018-10-24 16:23:24 +0200

    Bumb version to 1.5.4

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='Golem-Verificator',
-    version='1.5.2',
+    version='1.5.4',
     url='https://github.com/golemfactory/golem-verificator',
     maintainer='The Golem Team',
     maintainer_email='tech@golem.network',
