6f3ef0a3e67a96cbffad45d522db1a13e0cb96b7
Daniel Bigos <daniel.bigos@golem.network>
2018-10-25 12:32:08 +0200

    Refactor blender params

--- golem_verificator/blender_verifier.py
+++ golem_verificator/blender_verifier.py
@@ -52,26 +52,26 @@ class BlenderVerifier(FrameRenderingVerifier):
     def _get_part_size(self, subtask_info):
         if subtask_info['use_frames'] and len(subtask_info['all_frames']) \
           >= subtask_info['total_tasks']:
-            res_y = subtask_info['res_y']
+            res_y = subtask_info['resolution'][1]
         else:
             res_y = int(round(numpy.float32(
-                numpy.float32(subtask_info['crop_window'][2])
-                * numpy.float32(subtask_info['res_y']))))
-        return subtask_info['res_x'], res_y
+                numpy.float32(subtask_info['crops'][0]['borders_y'][0])
+                * numpy.float32(subtask_info['resolution'][1]))))
+        return subtask_info['resolution'][0], res_y
 
     @staticmethod
     def _get_part_size_from_subtask_number(subtask_info):
 
-        if subtask_info['res_y'] % subtask_info['total_tasks'] == 0:
-            res_y = int(subtask_info['res_y'] / subtask_info['total_tasks'])
+        if subtask_info['resolution'][1] % subtask_info['total_tasks'] == 0:
+            res_y = int(subtask_info['resolution'][1] / subtask_info['total_tasks'])
         else:
             # in this case task will be divided into not equal parts:
             # floor or ceil of (res_y/total_tasks)
             # ceiling will be height of subtasks with smaller num
-            ceiling_height = int(math.ceil(subtask_info['res_y'] /
+            ceiling_height = int(math.ceil(subtask_info['resolution'][1] /
                                            subtask_info['total_tasks']))
             additional_height = ceiling_height * subtask_info['total_tasks']
-            additional_pixels = additional_height - subtask_info['res_y']
+            additional_pixels = additional_height - subtask_info['resolution'][1]
             ceiling_subtasks = subtask_info['total_tasks'] - additional_pixels
 
             if subtask_info['start_task'] > ceiling_subtasks:
